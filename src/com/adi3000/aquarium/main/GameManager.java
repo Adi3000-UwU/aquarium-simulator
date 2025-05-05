7caea04a7f6702b067d2bb4298888c0d160dbdc4
adi3000 <adam.jan.gryglas@gmail.com>
2025-05-05 20:49:23 +0200

    Failed attempt to add group behaviour to the fish

--- src/com/adi3000/aquarium/main/GameManager.java
+++ src/com/adi3000/aquarium/main/GameManager.java
@@ -4,21 +4,46 @@ import com.adi3000.aquarium.math.Vector2;
 import com.adi3000.aquarium.objects.Fish;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GameManager {
     
-    Fish fish = new Fish(new Vector2(100, 200), new Color(0xE54848));
+    public ArrayList<Fish> fishes = new ArrayList<>();
     
     public GameManager() {
-    
+        
+        for (int i = 0; i < 100; i++) {
+            fishes.add(new Fish(new Vector2(Math.random() * Game.WIDTH, Math.random() * Game.HEIGHT), new Color(0xE54848)));
+        }
+        
     }
     
     
     public void tick() {
-        fish.tick();
+        for (Fish fish : fishes) {
+            fish.tick();
+            
+        }
+        
     }
     
     public void render(Graphics2D g2d) {
-        fish.render(g2d);
+        for (Fish fish : fishes) {
+            fish.render(g2d);
+        }
+        
     }
+    
+    
+    public ArrayList<Fish> getFishInRange(Vector2 position, double range) {
+        ArrayList<Fish> fishesInRange = new ArrayList<>();
+        
+        for (Fish fish : fishes) {
+            if (fish.position.distance(position) <= range) fishesInRange.add(fish);
+        }
+        
+        return fishesInRange;
+    }
+    
+    
 }
--- src/com/adi3000/aquarium/objects/Fish.java
+++ src/com/adi3000/aquarium/objects/Fish.java
@@ -1,9 +1,10 @@
 package com.adi3000.aquarium.objects;
 
-import com.adi3000.aquarium.main.MouseHandler;
+import com.adi3000.aquarium.main.Game;
 import com.adi3000.aquarium.math.Vector2;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Fish {
     
@@ -11,7 +12,7 @@ public class Fish {
     public Vector2 velocity;
     public Vector2 target;
     
-    private Color color;
+    public Color color;
     
     private double maxCelocity = 4;
     private double acceleration = 1;
@@ -20,22 +21,60 @@ public class Fish {
     public Fish(Vector2 position, Color color) {
         this.position = position;
         this.color = color;
-
+        
         velocity = new Vector2();
         target = new Vector2();
     }
     
     
     public void tick() {
-        target = MouseHandler.getInstance().mousePos;
+//        Vector2 newTarget = MouseHandler.getInstance().mousePos.add(new Vector2(Math.random() * 150 - 75, Math.random() * 150 - 75));
+//        if(newTarget.distance(target) > 120){
+//            target = newTarget;
+//        }
+//
+//        goToTarget(target);
+//
+        
+        
+        ArrayList<Fish> nearbyFishes = Game.gameManager.getFishInRange(position, 60);
+        
+        nearbyFishes.remove(this);
+        
+        if (nearbyFishes.size() > 1) {
+            Vector2 groupMiddle = new Vector2();
+            Vector2 averageVelocity = new Vector2();
+            for (Fish fish : nearbyFishes) {
+                groupMiddle.inc(fish.position);
+                averageVelocity.inc(fish.velocity);
+            }
+            groupMiddle.set(groupMiddle.div(nearbyFishes.size()));
+            averageVelocity.set(averageVelocity.div(nearbyFishes.size()));
+            
+            Vector2 newTarget = groupMiddle.add(averageVelocity.getNormilized());
+            
+            target = (newTarget.add(target).div(2).add(new Vector2(velocity.getNormilized().x * 10, 0)));
+            
+        } else {
+            target = new Vector2(Math.random() * Game.WIDTH, Math.random() * Game.HEIGHT);
+        }
+        
+        if (target.x < 0 || target.x > Game.WIDTH) {
+            target.x = Math.random() * Game.WIDTH;
+        }
+        if (target.y < 0 || target.y > Game.HEIGHT) {
+            target.y = Math.random() * Game.HEIGHT;
+        }
         
         goToTarget(target);
+        
+        
     }
     
     private void goToTarget(Vector2 target) {
-        velocity.inc(target.sub(position).setMagnitude(acceleration));
+        velocity.inc(target.sub(position).getNormilized().mult(acceleration));
         
-        if(velocity.mag() > maxCelocity) {
+        if (velocity.mag() > maxCelocity) {
             velocity.setMagnitude(maxCelocity);
         }
         
@@ -45,6 +84,9 @@ public class Fish {
     public void render(Graphics2D g2d) {
         g2d.setColor(color);
         g2d.fillOval((int) position.x, (int) position.y, 10, 10);
+        
+        g2d.setColor(Color.BLACK);
+//        g2d.fillOval((int) target.x, (int) target.y, 2, 2);
     }
     
 }
