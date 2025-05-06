7dcb9b6d0854f48b55263590d797ecd7a183a834
Adi3000 <adam.jan.gryglas@gmail.com>
2025-05-06 12:58:26 +0200

    feat: Add group behavior to fish

--- src/com/adi3000/aquarium/main/GameManager.java
+++ src/com/adi3000/aquarium/main/GameManager.java
@@ -12,7 +12,7 @@ public class GameManager {
     
     public GameManager() {
         
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 20; i++) {
             fishes.add(new Fish(new Vector2(Math.random() * Game.WIDTH, Math.random() * Game.HEIGHT), new Color(0xE54848)));
         }
         
--- src/com/adi3000/aquarium/objects/Fish.java
+++ src/com/adi3000/aquarium/objects/Fish.java
@@ -4,6 +4,7 @@ import com.adi3000.aquarium.main.Game;
 import com.adi3000.aquarium.math.Vector2;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Fish {
     
@@ -17,6 +18,11 @@ public class Fish {
     private double acceleration = 0.2;
     
     
+    public boolean isLeader = true;
+    public ArrayList<Fish> group = new ArrayList<>();
+    public Fish leader;
+    
+    
     public Fish(Vector2 position, Color color) {
         this.position = position;
         this.color = color;
@@ -27,9 +33,31 @@ public class Fish {
     
     
     public void tick() {
-        if (target.equals(new Vector2()) || position.distance(target) < 30) {
-            target = new Vector2(Math.random() * Game.WIDTH, Math.random() * Game.HEIGHT);
+        
+        if (isLeader) {
+            ArrayList<Fish> fishNearby = Game.gameManager.getFishInRange(position, 60);
+            fishNearby.remove(this);
+            if (!fishNearby.isEmpty()) {
+                fishNearby.get(0).joinGroup(this);
+            }
         }
+        
+        if (!isLeader && leader.group.size() > 10) {
+            leaveGroup();
+        }
+        
+        
+        if (target.equals(new Vector2()) || position.distance(target) < 20) {
+            if (isLeader) {
+                target.set(new Vector2(Math.random() * Game.WIDTH, Math.random() * Game.HEIGHT));
+            } else {
+                target.set(leader.target.add(position.sub(leader.position)).add(new Vector2(Math.random() * 20 - 10, Math.random() * 20 - 10)));
+            }
+        }
+        if (!isLeader && position.distance(leader.position) > 40) {
+            target.set(leader.target);
+        }
+        
         
         goToTarget(target);
     }
@@ -44,9 +72,52 @@ public class Fish {
         position.inc(velocity);
     }
     
+    private void joinGroup(Fish fish) {
+        if (fish.equals(this)) return;
+        
+        if (!isLeader) {
+            leader.joinGroup(fish);
+            return;
+        }
+        
+        group.add(fish);
+//        group.addAll(fish.group);
+//        for (Fish groupFish : fish.group) {
+//            groupFish.leader = this;
+//        }
+//        fish.group.clear();
+        fish.leaveGroup();
+        fish.isLeader = false;
+        fish.leader = this;
+    }
+    
+    private void leaveGroup() {
+        if (isLeader) {
+            if (!group.isEmpty()) {
+                Fish newLeader = group.get((int) (Math.random() * group.size()));
+                group.remove(newLeader);
+                newLeader.group.addAll(group);
+                group.clear();
+                newLeader.isLeader = true;
+            }
+            return;
+        }
+        leader.group.remove(this);
+        isLeader = true;
+    }
+    
     public void render(Graphics2D g2d) {
-        g2d.setColor(color);
+        if (isLeader) {
+            g2d.setColor(Color.GREEN);
+        } else {
+            g2d.setColor(color);
+//            g2d.drawLine((int) position.x, (int) position.y, (int) leader.position.x, (int) leader.position.y);
+        }
+        
         g2d.fillOval((int) position.x, (int) position.y, 10, 10);
+
+//        g2d.setColor(Color.MAGENTA);
+//        g2d.drawLine((int) position.x, (int) position.y, (int) target.x, (int) target.y);
     }
     
 }
